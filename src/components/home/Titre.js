e091ee1612f27b9ebb7b65db394b8452b4ad8a35
HaydenLeDev <loicroth.pro@gmail.com>
2022-11-17 18:20:18 +0100

    home fait

--- src/components/home/Titre.js
+++ src/components/home/Titre.js
@@ -6,7 +6,11 @@ function Titre() {
         <div className="Titre">
             <img src={backgroundLink} alt='background' className="Titre-image"></img>
             <span className="Titre-background"></span>
-            <h1 className="Titre-text">Chez vous, partout et ailleurs</h1>
+            <div className="Titre-text-layout">
+                <h1 className="Titre-text">Chez vous,</h1>
+                <h1 className="Titre-text">partout et ailleurs</h1>
+            </div>
+            
         </div>
     );
 }
