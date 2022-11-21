8be38c11a5464a629a6032ff7cc649c64811fdaa
HaydenLeDev <loicroth.pro@gmail.com>
2022-11-21 13:22:09 +0100

    modi card

--- src/components/home/GallerieItems.js
+++ src/components/home/GallerieItems.js
@@ -1,14 +1,16 @@
 import '../../style/components/home/GallerieItems.css';
+import { Link } from "react-router-dom"
 
 function Gallerie(props) {
-    //const id = props.id
+    const id = props.id
     const title = props.title
     const image = props.image
+    const link = "/logements/" + id
 
     return (
       <div className="Gallerie-Item">
+        <Link className="Gallerie-Item-background" to={link}></Link>
         <img src={image} alt="Presentation du Logement" className='Gallerie-Item-background-image'></img>
-        <div className="Gallerie-Item-background"></div>
         <h2 className="Gallerie-Item-title">{title}</h2>
       </div>
     );
