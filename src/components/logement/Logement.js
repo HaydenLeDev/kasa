32f9c516672f74af36cc55eb0fa6fe469ca905e2
HaydenLeDev <loicroth.pro@gmail.com>
2022-12-07 14:54:19 +0100

    Update Logement.js

--- src/components/logement/Logement.js
+++ src/components/logement/Logement.js
@@ -1,5 +1,6 @@
 import "../../style/components/logement/logement.css"
 import { useParams } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 import Data from '../../data/Data.json'
 import Carousel from './Carousel'
 import Tag from "./Tag"
@@ -7,24 +8,27 @@ import Title from "./Title"
 import DropDown from "../aPropos/DropDown"
 import Star from "./Star"
 import Profil from "./Profil"
-
 /**
  * An accommodation has a carousel with the images of the accommodation as well as the various information.
  * @param {*} props 
  * @returns the content of the accommodation page
  */
 
-function Logement(props) {
+function Logement() {
 
   const { id } = useParams()
   var logement
-  
+
   Data.map(data => {
     if (data.id === id) {
       logement = data
-    }
+    } 
     return null
   })
+
+  if (logement === undefined){
+     return <Navigate to="/error404" />
+  }
 
   const pictures = logement.pictures
   const equipments = logement.equipments.map((equipment) =>
